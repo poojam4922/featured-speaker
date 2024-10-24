e08465507a165e544902ba2737b6562e9d4d6764
poojam4922 <162030892+poojam4922@users.noreply.github.com>
2024-10-24 09:43:33 +0530

    revised  assesment completed

--- script.js
+++ script.js
@@ -1,67 +1,51 @@
 console.log("connected...");
 // for slider
 const slider = document.querySelector(".speakers-slider");
+const slider_wrapper = document.querySelector(".slider_wrapper");
 const leftButton = document.querySelector("#left-arrow");
 const rightButton = document.querySelector("#right-arrow");
-console.log(leftButton, "leftButton");
-console.log(rightButton, "rightButton");
-
-// slider.addEventListener("scroll", function () {
-//   console.log("scroll");
-//   // Show the left arrow when not at the very beginning
-//   if (slider.scrollLeft > 0) {
-//     leftButton.classList.remove("hidden");
-//   } else {
-//     leftButton.classList.add("hidden");
-//   }
-
-//   // Optionally, hide the right arrow if you've scrolled to the end
-//   if (slider.scrollWidth - slider.clientWidth === slider.scrollLeft) {
-//     rightButton.classList.add("hidden");
-//   } else {
-//     rightButton.classList.remove("hidden");
-//   }
-// });
-
 leftButton.addEventListener("click", function () {
+  console.log("leftButton clicked");
   slider.scrollBy({ left: -300, behavior: "smooth" });
 });
 
 rightButton.addEventListener("click", function () {
+  console.log("rightButton clicked");
   slider.scrollBy({ left: 300, behavior: "smooth" });
 });
 
 // show speaker detailes
 
 const speakerCard = document.querySelectorAll(".speaker-card");
-// console.log(speakerCard, "speaker card");
 
 speakerCard.forEach((card) => {
   card.addEventListener("click", () => {
+    if (slider_wrapper) {
+      slider_wrapper.style.marginTop = "2rem";
+    }
+    console.log("card clicked");
     const speakerName = card.getAttribute("data-name");
     const speakerRole = card.getAttribute("data-role");
     const speakerCompany = card.getAttribute("data-company");
     const speakerImage = card.getAttribute("data-image");
-    console.log(
-      speakerName,
-      speakerRole,
-      speakerCompany,
-      speakerImage,
-      "speakerDetails"
-    );
     // update speaker details
-    const detailSection = document.getElementById("speaker-detail");
+    const detailSection = document.querySelector(".speaker-detail");
     console.log(detailSection, "detailSection");
-    detailSection.querySelector("img").src = speakerImage;
-    detailSection.querySelector("h2").innerText = speakerName;
-    detailSection.querySelector(".text-sm-700").innerText = speakerRole;
-    detailSection.querySelector(".text-gray-500").innerText = speakerCompany;
+    document.getElementById("speaker-img").src = speakerImage;
+    document.getElementById("speaker-name").innerText = speakerName;
+    document.getElementById("speaker-role").innerText = speakerRole;
+    document.getElementById("speaker-company").innerText = speakerCompany;
 
-    detailSection.classList.remove("hidden");
+    detailSection.classList.remove("toggle");
   });
 });
 
 function hideDetails() {
-  const detailSection = document.getElementById("speaker-detail");
-  detailSection.classList.add("hidden");
+  console.log("hideDetails");
+  const detailSection = document.querySelector(".speaker-detail");
+  console.log(detailSection, "detailSection");
+  detailSection.classList.add("toggle");
+  if (slider_wrapper) {
+    slider_wrapper.style.marginTop = "6rem";
+  }
 }
